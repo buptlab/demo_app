10ccf1386853a1b1008251470af4446160a32290
buptlab <scstscst24@gmail.com>
2012-05-31 08:17:32 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3f2c4768ab7444e7f45aca4de5d0b10829fd4a479aeeece9d405d9d0d4fc924635b84f54d4d975e0265b5fe6a12c43e107cd6ba3928c21f5167ed3ac04b99ff4'
